cbc4e89af3a3da6e8f21ab93f5a3ff2e836cf3ba
Barry Chen <spchen38@163.com>
2016-10-26 23:26:46 +0800

    now have basic style to look

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -1,10 +1,14 @@
 $(function() {
 	
-	// confirm nickname
-	var nickname,
-		$confirm = $('.confirm'),
+	var $confirm = $('.confirm'),
 		$nickname = $('.nickname'),
-		$input = $('.input');
+		$input = $('.input'),
+		$inner = $('.messages .inner'),
+		$o_message = $('.messages .left'),
+		$message = $('.messages .right');
+	
+	var your_nickname = '<i>Guest</i>',
+		your_message = '<li>Hello World!' + your_nickname + '</li><br>';
 	
 	$confirm.on('click', function(event) {
 		event.preventDefault();
@@ -19,22 +23,47 @@ $(function() {
 	});
 	
 	$input.on('keydown', function(event) {
-		if (event.keyCode === 13) {
+		if (event.keyCode === 13 && !event.ctrlKey) {
 			event.preventDefault();
+			var new_left_height = $o_message.height();
+			sendMessage(new_left_height);
 		}
 		if (event.keyCode === 13 && event.ctrlKey) {
 			$(this).val($input.val() + '\n');
 		}
 	});
 	
+	$('.send').on('click', function() {
+		var new_left_height = $o_message.height();
+		sendMessage(new_left_height);
+	});
 	
 	function checkNN() {
 		var nn = $('.nickname').val();
 		if (nn === '') {
-			alert('Please enter you nickname!');
+			confirm('CAN YOU ENTER YOUR NICKNAME?');
 		} else {
-			nickname = nn;
+			$('.whoiam').html(nn);
+			your_nickname = '<i>' + nn + '</i>';
 			$('.enter_nickname').hide();
+		}
+	}
+	
+	function sendMessage(new_height) {
+		if ($input.val() === '') {
+			alert('CANNOT SEND BLANK MESSAGE!');
+		} else {
+			your_message = '<li>' + $input.val() + your_nickname + '</li><br>';
+			$message.append(your_message);
+			var left_height = $o_message.height(),
+				len = $message.children('li').length;
+			if (new_height !== left_height || left_height === 0) {
+				$message.children('li').eq(len - 1).css({
+					'margin-top': left_height + 24
+				});
+			}
+			$inner.scrollTop($message.height());
+			$input.val('');
 		}
 	}
 	
