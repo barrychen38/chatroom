2e701950babeb33d312ebe4065fcd10e20c3a96e
Barry Chen <spchen38@163.com>
2016-11-19 19:33:13 +0800

    change view engine html => jade

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 var express = require('express'),
 	http = require('http'),
 	bodyParser = require('body-parser'),
-	ejs = require('ejs'),
 	jade = require('jade'),
 	mysql = require('mysql'),
 	favicon = require('serve-favicon'),
@@ -24,9 +23,9 @@ var app = express(),
 	server = http.createServer(app),
 	io = require('socket.io')(server);
 // engine
-app.engine('html', ejs.__express);
+// app.engine('html', ejs.__express);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'html');
+app.set('view engine', 'jade');
 // favicon
 app.use(favicon(__dirname + '/public/favicon.ico'));
 // static files
