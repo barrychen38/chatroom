7b3a5bb533cfb9dce987c6450284ecc33bce77fa
Barry Chen <spchen38@163.com>
2016-12-23 14:35:42 +0800

    deploy on heroku to find some bug and fixed

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ const fs = require('fs');
 const chat = require('./routes/chat');
 // config
 const mysql_config = require('./config/config').config;
-// utils
-const errMsg = require('./utils/errmsg');
-const sqlQuery = require('./utils/query');
 // port
 const PORT = process.env.PORT || 2261;
 // server
@@ -40,79 +37,6 @@ app.all('*', (req, res, next) => {
 	res.header("Access-Control-Allow-Methods", "GET,POST");
 	// res.header("Content-Type", "application/json;charset=utf-8");
 	next();
-});
-// create mysql pool
-let pool = mysql.createPool(mysql_config);
-// get info
-app.get('/getInfo', (req, res) => {
-	pool.getConnection((err, connection) => {
-		if (err) {
-			res.send(JSON.stringify(errMsg.sql_connect_error));
-			return;
-		}
-		connection.query(sqlQuery.select_user, (err, rows) => {
-			if (err) {
-				res.send(JSON.stringify(errMsg.sql_select_error));
-				return;
-			}
-			res.send(JSON.stringify({result: 1, data: rows}));
-			connection.release();
-		});
-	});
-});
-// login
-app.post('/loginConfirm', (req, res) => {
-	let request = req.body;
-	pool.getConnection((err, connection) => {
-		if (err) {
-			res.send(JSON.stringify(errMsg.sql_connect_error));
-			return;
-		}
-		connection.query(sqlQuery.select_user_where_email, [request.name], (err, rows) => {
-			if (err) {
-				res.send(JSON.stringify(errMsg.sql_select_error));
-				return;
-			}
-			if (rows.length) {
-				if (rows[0].pwd === request.password) {
-					res.send(JSON.stringify({result: 1}));
-				} else {
-					res.send(JSON.stringify({result: 3}));
-				}
-			} else {
-				res.send(JSON.stringify({result: 2}));
-			}
-			connection.release();
-		});
-	});
-});
-// register
-app.post('/register', (req, res) => {
-	let request = req.body,
-		values = [request.name, request.mobile, request.password, request.email, request.uid];
-	pool.getConnection((err, connection) => {
-		if (err) {
-			res.send(JSON.stringify(errMsg.sql_connect_error));
-			return;
-		}
-		connection.query('SELECT mobile,email FROM user WHERE uid = ?', [request.uid], (err, rows) => {
-			if (rows.length) {
-				connection.query('UPDATE user SET name = ?, mobile = ?, pwd = ?, email = ? WHERE uid = ?', values, (err, results) => {
-					if (err) {
-						res.send(JSON.stringify(errMsg.sql_update_error));
-						return;
-					}
-					res.send(JSON.stringify({result: 2}));
-					connection.release();
-				});
-			} else {
-				connection.query('INSERT INTO user VALUES(?, ?, ?, ?, ?)', values, (err, result) => {
-					res.send(JSON.stringify({result: 1}));
-					connection.release();
-				});
-			}
-		});
-	});
 });
 // chat
 let p_count = 0,
@@ -161,41 +85,19 @@ io.on('connection', socket => {
 });
 // save image return to client
 app.post('/upload_image', (req, res) => {
-	let r = req.body;
-	
+	let r = req.body,
+		data = null;
 	let ext = r.file.substr(0, 22).match(/(jpg|jpeg|png|gif)/)[0],
 		file = r.file.substr(22);
 	let file_name = uuid.v1({msec: new Date().getTime()}) + '.' + ext;
 	
 	fs.writeFile('public/upload/' + file_name, new Buffer(file, 'base64'), err => {
 		if (err) {
-			res.send(JSON.stringify({readyState: 0, msg: 'Upload failed.'}));
-			return;
+			data = JSON.stringify({readyState: 0, msg: 'Upload failed.'});
 		}
+		data = JSON.stringify({readyState: 1, img_url: '/upload/' + file_name});
+		res.send(data);
 	});
-	res.send(JSON.stringify({readyState: 1, img_url: '/upload/' + file_name}));
-});
-// get chat history
-app.get('/get_chat_history', (req, res) => {
-	pool.getConnection((err, connection) => {
-		if (err) {
-			res.send(JSON.stringify(errMsg.sql_connect_error));
-			return;
-		}
-		connection.query(sqlQuery.select_save_chat, (err, rows) => {
-			if (err) {
-				res.send(JSON.stringify(errMsg.sql_select_error));
-				return;
-			}
-			res.send(JSON.stringify({result: 1, data: rows}));
-			connection.release();
-		});
-	});
-	
-});
-// save chat
-app.post('/save_chat', (req, res) => {
-	
 });
 // run server
 server.listen(PORT, () => {
